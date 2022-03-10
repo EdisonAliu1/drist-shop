d887cc18bfdfa3c23977542aec8bfbf7e572fa1e
EdisonAliu1 <ea48304@ubt-uni.net>
2022-03-10 04:05:11 +0100

    First Changes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "bootstrap";
+import jQuery from "jquery";
+window.$ = window.jQuery = jQuery;
+import "@popperjs/core";
+import "./assets/app.scss";
 
 createApp(App).use(router).mount("#app");
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,5 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <div class="home container"></div>
 </template>
 
 <script>
@@ -12,6 +9,7 @@ import HelloWorld from "@/components/HelloWorld.vue";
 export default {
   name: "HomeView",
   components: {
+    // eslint-disable-next-line vue/no-unused-components
     HelloWorld,
   },
 };
